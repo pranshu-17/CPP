24926931287ea935685d1e145f93faf355488fdc
Pranshu Bhagat <133300555+pranshu-17@users.noreply.github.com>
2023-08-16 03:15:28 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
