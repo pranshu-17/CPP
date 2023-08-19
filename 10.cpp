6442f159f9f821770628cbd4e675df15c53196a2
Pranshu Bhagat <133300555+pranshu-17@users.noreply.github.com>
2023-08-19 15:56:34 +0530

    new files added

---
+++ 10.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int n = 5;
+    for (int i = 1; i <= 2 * n - 1; i++)
+    {
+        int stars = i;
+        if (i > n)
+        {
+            stars = 2 * n - i;
+        }
+
+        for (int j = 0; j < stars; j++)
+        {
+            cout << "*";
+        }
+
+        cout << "\n";
+    }
+
+    return 0;
+}
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int i, j, k, l;
+    for (i = 0; i < 5; i++)
+    {
+        for (j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+    for (k = 0; k < 4; k++)
+    {
+        for (l = 0; l < 4 - k; l++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
+*/
