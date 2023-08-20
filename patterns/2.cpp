0e63a05c8cf5313a0494d5904865541b98f5285e
himanshu <vinitabg2@gmail.com>
2023-08-20 22:56:03 +0530

    pattern folder created

---
+++ patterns/2.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
