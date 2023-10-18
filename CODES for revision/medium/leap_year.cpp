5e389a7f583253d018d693a2c0bb995f4c7b5a48
himanshu <vinitabg2@gmail.com>
2023-10-18 16:31:04 +0530

    new

--- CODES for revision/medium/leap_year.cpp
+++ CODES for revision/medium/leap_year.cpp
@@ -5,7 +5,7 @@ class Solution {
 public:
     bool leapyear(int year) {
         // Your code goes here
-        if(year%400 ==0){  
+        if(year%400 ==0){   
         return true; 
         } 
         else if(year%4==0 && year%100 != 0){
