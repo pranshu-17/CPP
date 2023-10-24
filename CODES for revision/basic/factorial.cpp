432ce79f66b41d3f743dbb7aff7d5fc94f0c1340
himanshu <vinitabg2@gmail.com>
2023-10-24 14:05:24 +0530

    new

--- CODES for revision/basic/factorial.cpp
+++ CODES for revision/basic/factorial.cpp
@@ -3,7 +3,7 @@ class Solution {
 public: 
 	long factorial(int fact) {
 		// Your code goes here 
-        if (fact <= 1) { 
+        if (fact <= 1) {  
             return 1;   
         }  
         return fact * factorial(fact - 1); 
