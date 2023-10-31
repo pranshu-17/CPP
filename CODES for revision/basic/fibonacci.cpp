aa962fbb42d6dbcecf707eaef6e259aa3ff3f5dd
himanshu <vinitabg2@gmail.com>
2023-10-31 10:54:59 +0530

    new

--- CODES for revision/basic/fibonacci.cpp
+++ CODES for revision/basic/fibonacci.cpp
@@ -8,7 +8,7 @@ public:
         int first = 0, second = 1, next;
         for (int i = 0; i < n; i++) {
             if (i <= 1) 
-            {
+            { 
             next = i; 
             } else {
                 next = first + second;
