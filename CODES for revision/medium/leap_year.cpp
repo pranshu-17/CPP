9a5b89a8d4f3f4eda50269ea76032a4f2b1bd0b2
himanshu <vinitabg2@gmail.com>
2023-11-18 18:04:22 +0530

    new

--- CODES for revision/medium/leap_year.cpp
+++ CODES for revision/medium/leap_year.cpp
@@ -7,7 +7,7 @@ public:
         return true; 
         } 
         else if(year%4==0 && year%100 != 0){ 
-            return true;
+            return true; 
         }
         else return false;
             } 
