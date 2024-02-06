42642e2b74969f5d56f85307242d7931c22b682c
himanshu <vinitabg2@gmail.com>
2024-02-06 09:28:43 +0530

    new

--- patterns/4.cpp
+++ patterns/4.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main()
 {
     for (int i = 1; i <= 5; i++)
-    { 
+    {  
         for (int j = 1; j <= i; j++)
         {
       
             cout << i << " ";  
         cout << "\n";
     }   
-     
+      
     return 0; s
 }  
