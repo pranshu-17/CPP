290397abb3f898ec226a2a461b9b8897ce29ea16
himanshu <vinitabg2@gmail.com>
2024-02-08 19:47:16 +0530

    new

--- patterns/10.cpp
+++ patterns/10.cpp
@@ -7,7 +7,7 @@ int main()
     int n = 5;
     for (int i = 1; i <= 2 * n - 1; i++)
     {
-        int stars = i;
+        int stars = i; 
         if (i > n) 
         {
             stars = 2 * n - i;
