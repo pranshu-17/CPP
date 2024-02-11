f9a6fb12c4298213bea3eb947ccb0adb26b405e0
himanshu <vinitabg2@gmail.com>
2024-02-11 14:54:23 +0530

    new

--- patterns/2.cpp
+++ patterns/2.cpp
@@ -3,12 +3,12 @@ using namespace std;
   
 int main()  
 {
-
+ 
     for (int i = 0; i < 8; i++) 
     {
         for (int j = 0; j < i; j++)
         {
-            cout << "* ";
+            cout << "* "; 
         }
         cout << "\n"; 
     }
