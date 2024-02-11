c55b1ec577d3556d44057de9b380a59cbb805f76
himanshu <vinitabg2@gmail.com>
2024-02-11 14:54:00 +0530

    new

--- patterns/2.cpp
+++ patterns/2.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() 
+int main()  
 {
 
-    for (int i = 0; i < 8; i++)
+    for (int i = 0; i < 8; i++) 
     {
         for (int j = 0; j < i; j++)
         {
