7882a9ae28a128f54d756361fc68097c0b7ee606
himanshu <vinitabg2@gmail.com>
2024-02-12 19:55:21 +0530

    new

--- patterns/4.cpp
+++ patterns/4.cpp
@@ -1,16 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
+int main() 
 {
     for (int i = 1; i <= 5; i++)
     {  
         for (int j = 1; j <= i; j++)
         {
-      
+       
             cout << i << " ";  
         cout << "\n";
-    }    
-      
+    }     
+       
     return 0; s 
 }     
