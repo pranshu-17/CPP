e92cb0a279c4c2fa2404bbcc8cf1ef33857bb8ad
himanshu <vinitabg2@gmail.com>
2024-02-21 20:56:48 +0530

    new

--- patterns/9.cpp
+++ patterns/9.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
+int main() 
 {  
  
     for (int i = 1; i <= 5; i++)
-    {
+    { 
         for (int j = 1; j <= 5 - i; j++)
         { 
             cout << " ";
