98f540e8f5db2ad4d04e2d9a48c6f5720bb2f3b2
himanshu <vinitabg2@gmail.com>
2024-02-22 09:05:46 +0530

    new

--- patterns/6.cpp
+++ patterns/6.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;   
- 
+  
 int main()
-{
+{ 
     for (int i = 1; i <= 5; i++) 
     { 
         for (int j = 1; j <= 6 -  i; j++)  
--- patterns/9.cpp
+++ patterns/9.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() 
-{  
+{   
   
     for (int i = 1; i <= 5; i++) 
     {  
