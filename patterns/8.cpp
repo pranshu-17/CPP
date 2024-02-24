fc6eac61a54815809ca2d8f212f82df85daa752f
himanshu <vinitabg2@gmail.com>
2024-02-24 19:11:58 +0530

    new

--- patterns/8.cpp
+++ patterns/8.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
+int main() 
 {
     int i, j, k;
     for (i = 1; i <= 5; i++)  
     {
         for (j = 1; j <= i - 1; j++)
         {
-            cout << " ";
+            cout << " "; 
         }
         for (k = 1; k <= 10 - 2 * i + 1; k++) // 2n-(2i+1)  formula !
         {
