e62584b69fde1aa9573628349ce1862df39de8f7
himanshu <vinitabg2@gmail.com>
2024-03-01 13:08:17 +0530

    new

--- patterns/5.cpp
+++ patterns/5.cpp
@@ -5,7 +5,7 @@ int main()
 {
     for (int i = 0; i < 5; i++)
     {
-        for (int j = 0; j < 5 - i; j++)    
+        for (int j = 0; j < 5 - i; j++)
         {
             cout << "* ";    
         }   
