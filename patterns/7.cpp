39c85e0b0700a51d0093e5781ef703252786a8dd
himanshu <vinitabg2@gmail.com>
2024-03-08 13:11:31 +0530

    new

--- patterns/7.cpp
+++ patterns/7.cpp
@@ -3,12 +3,12 @@ using namespace std;
  
 int main()     
 {   
-    for (int i = 1; i <= 5; i++)  
+    for (int i = 1; i <= 5; i++)   
     {  
         for (int j = 1; j <= 5 - i; j++)    
         {   
             cout << " ";
-        }      
+        }
         for (int k = 1; k <= 2 * i - 1; k++)    
         {   
             cout << "*";      
