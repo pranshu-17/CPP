4f42bdd70a0558404896f27414383cd10a9d05de
himanshu <vinitabg2@gmail.com>
2024-03-20 15:23:19 +0530

    new

--- patterns/7.cpp
+++ patterns/7.cpp
@@ -12,9 +12,9 @@ int main()
         for (int k = 1; k <= 2 * i - 1; k++)  
         {    
             cout << "*";        
-          }     
+          }      
          
-        cout << "\n" ; 
+         cout << "\n" ; 
     }    
 
     return 0;    
