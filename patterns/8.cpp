1ce07c688a6dea2c8ffed77726110ddd848a4352
himanshu <vinitabg2@gmail.com>
2024-03-22 03:03:12 +0530

    new

--- patterns/8.cpp
+++ patterns/8.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() 
 {   
-    int i, j, k;
+    int i, j, k; 
     for (i = 1; i <= 5; i++)    
     {  
         for (j = 1; j <= i - 1; j++) 
@@ -11,11 +11,11 @@ int main()
             cout << " "; 
         }
         for (k = 1; k <= 10 - 2 * i + 1; k++) // 2n-(2i+1)  formula ! 
-        {    
+        {     
             cout << "*";   
         }  
         cout << "\n"; 
-    }   
+    }    
  
     return 0;
 }  
