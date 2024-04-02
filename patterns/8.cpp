9ee3c2833525746db47691823cab2a1759b4a5e0
himanshu <vinitabg2@gmail.com>
2024-04-02 22:50:48 +0530

    new

--- patterns/8.cpp
+++ patterns/8.cpp
@@ -7,7 +7,7 @@ int main()
     for (i = 1; i <= 5; i++)     
     {   
         for (j = 1; j <= i - 1; j++)  
-        {    
+        {     
             cout << " ";   
         } 
         for (k = 1; k <= 10 - 2 * i + 1; k++) // 2n-(2i+1)  formula ! 
