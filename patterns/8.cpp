1c91b21c8cd06ff8c24e4e6308d53cbb09466e9f
himanshu <vinitabg2@gmail.com>
2024-04-02 22:50:39 +0530

    new

--- patterns/8.cpp
+++ patterns/8.cpp
@@ -6,7 +6,7 @@ int main()
     int i, j, k; 
     for (i = 1; i <= 5; i++)     
     {   
-        for (j = 1; j <= i - 1; j++) 
+        for (j = 1; j <= i - 1; j++)  
         {    
             cout << " ";   
         } 
