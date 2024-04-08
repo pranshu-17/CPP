102d347f5e99482f7813640981dcc98f820abd74
himanshu <vinitabg2@gmail.com>
2024-04-08 22:21:17 +0530

    new

--- patterns/9.cpp
+++ patterns/9.cpp
@@ -6,15 +6,15 @@ int main()
   
     for (int i = 1; i <= 5; i++)  
     {    
-        for (int j = 1; j <= 5 - i; j++)   
+        for (int j = 1; j <= 5 - i; j++)    
         {   
-            cout << " "; 
+            cout << " ";  
         }    
         for (int k = 1; k <= 2 * i - 1; k++)  
         {    
-            cout << "*";   
+            cout << "*";    
         }      
- 
+  
         cout << "\n"; 
     }    
     int i, j, k; s  
