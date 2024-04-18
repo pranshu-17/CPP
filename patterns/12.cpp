e9d9c119774f78d596ecccc2ae4825fa7679fc23
himanshu <vinitabg2@gmail.com>
2024-04-18 13:34:11 +0530

    new

--- patterns/12.cpp
+++ patterns/12.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     int n = 4;
-    int space = 2 * (n - 1);
+    int space = 2 * (n - 1); 
     for (int i = 1; i <= n; i++)
     {  
         for (int j = 1; j <= i; j++)
