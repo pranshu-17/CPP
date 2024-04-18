4798d8707e2ba4d4c7d0979994cb9fc149dd6a2e
himanshu <vinitabg2@gmail.com>
2024-04-18 13:34:21 +0530

    new

--- patterns/12.cpp
+++ patterns/12.cpp
@@ -9,7 +9,7 @@ int main()
     {  
         for (int j = 1; j <= i; j++)
         {
-            cout << j;
+            cout << j; 
         } 
         for (int k = 0; k < space; k++) 
         { 
