5c445c234c5e202c98cd55a0bde5f2e406f9af74
himanshu <vinitabg2@gmail.com>
2024-04-24 18:40:42 +0530

    new

--- patterns/5.cpp
+++ patterns/5.cpp
@@ -6,10 +6,10 @@ int main()
     for (int i = 0; i < 5; i++)   
     {
         for (int j = 0; j < 5 - i; j++)   
-        { 
+        {  
             cout << "* ";  
         }   
         cout << "\n";      
-  
+   
     return 0;  
 }         
