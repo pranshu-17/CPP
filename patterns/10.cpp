4d6b8ae31134cf5dc550c1b35ad95b1d58292b36
himanshu <vinitabg2@gmail.com>
2024-05-02 00:38:44 +0530

    new

--- patterns/10.cpp
+++ patterns/10.cpp
@@ -3,12 +3,12 @@ using namespace std;
  
 int main() 
 {   
-
+ 
     int n = 5;  
     for (int i = 1; i <= 2 * n - 1; i++)  
     {
         int stars = i;  
-        if (i > n) 
+        if (i > n)  
         {  
             stars = 2 * n  - i; 
         }  
