7ebf68a0a0e95ef3dad66a900ae8fd165be897dd
himanshu <vinitabg2@gmail.com>
2024-05-08 00:19:47 +0530

    new

--- patterns/12.cpp
+++ patterns/12.cpp
@@ -6,20 +6,20 @@ int main()
     int n = 4;
     int space = 2 * (n - 1); 
     for (int i = 1; i <= n; i++)
-    {   
+    {    
         for (int j = 1; j <= i; j++)
         {
             cout << j;   
-        }  
-        for (int k = 0; k < space; k++)  
+        }   
+        for (int k = 0; k < space; k++)   
         { 
             cout << " ";    
         }    
         for (int l = i; l >= 1; l--)
         { 
             cout << l;
-        }
-
+        } 
+ 
         cout << "\n";
         space -= 2;
     }
